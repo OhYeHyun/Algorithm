75eb0f3385ac421463f7d0424ec7b845c20a6892
OhYeHyun <107532928+OhYeHyun@users.noreply.github.com>
2022-12-13 20:04:50 +0900

    [Bronze V] Title: 새싹, Time: 116 ms, Memory: 9324 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.js
@@ -0,0 +1,13 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+
+solution();
+
+function solution() {
+  console.log(`         ,r'"7
+r\`-_   ,'  ,/
+ \\. ". L_r'
+   \`~\\/
+      |
+      |`);
+}
