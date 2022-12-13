c9f6c9401e0cc2fe8c34fafa683ec620988b41f3
OhYeHyun <107532928+OhYeHyun@users.noreply.github.com>
2022-12-13 19:45:05 +0900

    [Bronze V] Title: 고양이, Time: 116 ms, Memory: 9320 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.js
@@ -0,0 +1,11 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+
+solution();
+
+function solution() {
+  console.log(`\\    /\\
+ )  ( ')
+(  /  )
+ \\(__)|`);
+}
