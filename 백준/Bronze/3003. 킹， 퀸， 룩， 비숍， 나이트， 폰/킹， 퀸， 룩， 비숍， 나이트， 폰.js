c1bd17bd4a21ca7ba0ced620ac78528b0e8740b4
OhYeHyun <107532928+OhYeHyun@users.noreply.github.com>
2022-12-13 18:35:04 +0900

    [Bronze V] Title: 킹, 퀸, 룩, 비숍, 나이트, 폰, Time: 128 ms, Memory: 9352 KB -BaekjoonHub

---
+++ 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.js
@@ -0,0 +1,17 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+let input = fs.readFileSync(filePath).toString().split("\n");
+
+input = input[0];
+input = input.split(" ").map((item) => +item);
+
+solution(input);
+
+function solution(a) {
+  const arr = [1, 1, 2, 2, 2, 8];
+  const newArr = [];
+  for (let i = 0; i < a.length; i++) {
+    newArr.push(arr[i] - a[i]);
+    console.log(newArr[i]);
+  }
+}
