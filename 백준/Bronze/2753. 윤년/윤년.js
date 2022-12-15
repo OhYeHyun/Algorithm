985b3f5ad82cc589f0dcb3217315919002075c68
OhYeHyun <107532928+OhYeHyun@users.noreply.github.com>
2022-12-15 16:22:54 +0900

    [Bronze V] Title: 윤년, Time: 136 ms, Memory: 9584 KB -BaekjoonHub

--- 백준/Bronze/2753. 윤년/윤년.js
+++ 백준/Bronze/2753. 윤년/윤년.js
@@ -8,16 +8,10 @@ input = input.split(" ").map((item) => +item);
 solution(input);
 
 function solution(a) {
-  if (a % 4 === 0) {
-    if (a % 100 === 0) {
-      if (a % 400 === 0) {
-        console.log("1");
-      } else {
-        console.log("0");
-      }
-    } else {
-      console.log("1");
-    }
+  if (a % 400 === 0) {
+    console.log("1");
+  } else if (a % 4 === 0 && a % 100 !== 0) {
+    console.log("1");
   } else {
     console.log("0");
   }
