29a68c149822d8a5cd60b9402ef62a512c51237d
OhYeHyun <107532928+OhYeHyun@users.noreply.github.com>
2022-12-19 16:25:21 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 128 ms, Memory: 9512 KB -BaekjoonHub

--- 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.js
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.js
@@ -6,9 +6,6 @@ solution(input[0]);
 
 function solution(a) {
   for (let i = 1; i <= a; i++) {
-    for (let j = 1; j <= i; j++) {
-      process.stdout.write("*");
-    }
-    console.log();
+    console.log("*".repeat(i));
   }
 }
