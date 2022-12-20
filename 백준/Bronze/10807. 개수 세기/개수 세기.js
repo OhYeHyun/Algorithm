5f20805db610008b6869fac44b584a326cc6ed48
OhYeHyun <107532928+OhYeHyun@users.noreply.github.com>
2022-12-20 15:56:31 +0900

    [Bronze V] Title: 개수 세기, Time: 140 ms, Memory: 9356 KB -BaekjoonHub

--- 백준/Bronze/10807. 개수 세기/개수 세기.js
+++ 백준/Bronze/10807. 개수 세기/개수 세기.js
@@ -3,15 +3,11 @@ const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
 let input = fs.readFileSync(filePath).toString().trim().split("\n");
 
 solution(
-  +input[0],
   input[1].split(" ").map((item) => +item),
   +input[2]
 );
 
-function solution(n, arr, num) {
-  let count = 0;
-  for (let i = 0; i < n; i++) {
-    arr[i] === num ? count++ : count;
-  }
+function solution(arr, num) {
+  let count = arr.filter((item) => item === num).length;
   console.log(count);
 }
