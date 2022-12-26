31e7dd3795aac6b24ebbf5b4c4636f87d1a37dae
OhYeHyun <107532928+OhYeHyun@users.noreply.github.com>
2022-12-26 18:24:20 +0900

    [Bronze V] Title: 알파벳 찾기, Time: 144 ms, Memory: 9448 KB -BaekjoonHub

---
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.js
@@ -0,0 +1,14 @@
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+let input = require("fs").readFileSync(filePath).toString().trim().split("\n");
+
+solution(input[0]);
+
+function solution(a) {
+  for (let i = 97; i <= 122; i++) {
+    if (a.includes(String.fromCharCode([i]))) {
+      console.log(a.indexOf(String.fromCharCode([i])));
+    } else {
+      console.log(-1);
+    }
+  }
+}
