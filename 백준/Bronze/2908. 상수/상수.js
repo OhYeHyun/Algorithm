801e738acc1cb121bab44cc7f3a6dbc28f77c75a
OhYeHyun <107532928+OhYeHyun@users.noreply.github.com>
2023-02-01 18:23:13 +0900

    [Bronze II] Title: 상수, Time: 124 ms, Memory: 9344 KB -BaekjoonHub

--- 백준/Bronze/2908. 상수/상수.js
+++ 백준/Bronze/2908. 상수/상수.js
@@ -5,17 +5,9 @@ let input = require("fs")
   .trim()
   .split(/ |\n/);
 
-solution(input[0].split(""), input[1].split(""));
+solution(input);
 
-function solution(num1, num2) {
-  const newNum1 = [];
-  const newNum2 = [];
-  num1.forEach(() => newNum1.push(num1.pop()));
-  newNum1.push(num1.pop());
-  num2.forEach(() => newNum2.push(num2.pop()));
-  newNum2.push(num2.pop());
-
-  console.log(
-    newNum1.join("") > newNum2.join("") ? newNum1.join("") : newNum2.join("")
-  );
+function solution(arr) {
+  const [newNum1, newNum2] = arr.map((e) => [...e].reverse().join(""));
+  console.log(Math.max(newNum1, newNum2));
 }
