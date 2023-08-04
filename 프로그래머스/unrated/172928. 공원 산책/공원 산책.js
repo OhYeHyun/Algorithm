1e2e6e56fa5f2c7e7b3811ee930ee3c3cbec8373
OhYeHyun <107532928+OhYeHyun@users.noreply.github.com>
2023-08-04 18:14:38 +0900

    [unrated] Title: 공원 산책, Time: 0.30 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/unrated/172928. 공원 산책/공원 산책.js
+++ 프로그래머스/unrated/172928. 공원 산책/공원 산책.js
@@ -1,3 +1,65 @@
+
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+19
+20
+21
+22
+23
+24
+25
+26
+27
+28
+29
+30
+31
+32
+33
+34
+35
+36
+37
+38
+39
+40
+41
+42
+43
+44
+45
+46
+47
+48
+49
+50
+51
+52
+53
+54
+55
+56
+57
+58
+59
+60
+61
+62
 function solution(park, routes) {
     let result = [];
     park.forEach((e) => {
@@ -5,13 +67,13 @@ function solution(park, routes) {
             e[i] == "S" ? result = [park.indexOf(e), e.indexOf("S")] : -1;
         }
     })
-    
+
     routes.forEach((e) => {
         let num = Number(e[2]);
         let direction = e[0];
         let i;
         let backUp = [result[0], result[1]];
-        
+
         if (direction == "N" && result[0] >= num) {
             for (i = 1; i <= num; i++) {
                 if (park[result[0] - 1][result[1]] == "O" || park[result[0] - 1][result[1]] == "S") {
