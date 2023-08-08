b743dff5a9a33d28023408f9ab6c65d285ee5c46
OhYeHyun <107532928+OhYeHyun@users.noreply.github.com>
2023-08-08 17:42:44 +0900

    [unrated] Title: 대충 만든 자판, Time: 5.35 ms, Memory: 33.7 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/160586. 대충 만든 자판/대충 만든 자판.js
@@ -0,0 +1,28 @@
+function solution(keymap, targets) {
+    let concat = new Map();
+    let answer = [];
+    let cnt = 0;
+    keymap.forEach((e) => {
+        [...e].forEach((el, index) => {
+            if (concat.has(el)) {
+                concat.get(el) > index ? concat.set(el, index) : -1;
+            } else {
+                concat.set(el, index);
+            }
+        })
+    })
+    console.log(concat)
+    targets.forEach((e) => {
+        cnt = 0;
+        [...e].forEach((el, index) => {
+            if (concat.has(el)) {
+                cnt == -1 ? cnt = -1 : cnt += concat.get(el) + 1;
+            } else {
+                cnt = -1;
+            }  
+        })
+        answer.push(cnt);
+    })
+    console.log(answer)
+    return answer;
+}
