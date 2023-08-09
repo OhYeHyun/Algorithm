f2158cccc99a9edf2c6d893e5a917c8401dcf3f1
OhYeHyun <107532928+OhYeHyun@users.noreply.github.com>
2023-08-09 20:00:11 +0900

    [unrated] Title: 카드 뭉치, Time: 0.11 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/159994. 카드 뭉치/카드 뭉치.js
+++ 프로그래머스/unrated/159994. 카드 뭉치/카드 뭉치.js
@@ -1,43 +1,27 @@
 function solution(cards1, cards2, goal) {
     let order = [];
-    let answer = "No";
+    const cards = [cards1, cards2];
+    let answer = "Yes";
     
-    cards1.forEach((e) => {
-        order.push(goal.indexOf(e))
-    })
-    while (order[order.length - 1] == -1) {
-        order.splice(order[order.length - 1], 1)
-    }
-    if (order.includes(-1)) return answer
-    console.log(order)
-    
-    let orderSorted = [...order];
-    orderSorted.sort((a, b) => a - b);
-    
-    if (orderSorted.join('') !== order.join('')) {
-        return answer;
-    } else {
-        order = [];
-        orderSorted = [];
-    } 
-    
-    cards2.forEach((e) => {
-        order.push(goal.indexOf(e))
-        order = order.filter((el) => el >= 0)
-    })
-    while (order[order.length - 1] == -1) {
-        order.splice(order[order.length - 1], 1)
-    }
-    if (order.includes(-1)) return answer
-    
-    orderSorted = [...order];
-    orderSorted.sort((a, b) => a - b);
+    cards.forEach((e) => {
+        e.forEach((el) => {
+            order.push(goal.indexOf(el))
+        })
+        while (order[order.length - 1] == -1) {
+            order.splice(order[order.length - 1], 1)
+        }
+        if (order.includes(-1)) answer = "No";
 
-    if (orderSorted.join('') !== order.join('')) {
-        return answer;
-    } else {
-        answer = "Yes"
-    }
+        let orderSorted = [...order];
+        orderSorted.sort((a, b) => a - b);
+
+        if (orderSorted.join('') !== order.join('')) {
+            answer = "No";
+        } else {
+            order = [];
+            orderSorted = [];
+        }
+    })
     return answer;
     
 }
