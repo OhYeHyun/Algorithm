17393da5321e36b643b21c078b908500e607ee1b
OhYeHyun <107532928+OhYeHyun@users.noreply.github.com>
2023-08-11 19:24:59 +0900

    [unrated] Title: 개인정보 수집 유효기간, Time: 0.37 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/unrated/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.js
+++ 프로그래머스/unrated/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.js
@@ -1,46 +1,19 @@
 function solution(today, terms, privacies) {
     let term = new Map();
     let answer = [];
-    today = today.split(".").map(Number);
+    today = today.split(".");
+    
+    const total = Number(today[0]) * 12 * 28 + Number(today[1]) * 28 + Number(today[2]);
     terms.forEach((e) => {
         e = e.split(" ");
         term.set(e[0], e[1]);
     });
     privacies.forEach((e, idx) => {
-        e = e.split(" ");
-        let until = day(e[0], term.get(e[1]));
+        e = e.split(/\.| /);
+        let until = Number(e[0]) * 12 * 28 + Number(e[1]) * 28 + Number(e[2]) - 1 + term.get(e[3]) * 28;
         
-        if (until[0] < today[0]) {
-            answer.push(idx + 1);
-        } else if (until[0] == today[0]) {
-            if (until[1] < today[1]) {
-                answer.push(idx + 1);
-            } else if (until[1] == today[1]) {
-                if (until[2] < today[2]) {
-                    answer.push(idx + 1);
-                }
-            }
-        }
-        
+        until < total ? answer.push(idx + 1) : -1;
         
     });
     return answer;
-    
-    function day(d, plus) {
-        plus = plus * 28 - 1;
-        d = d.split(".").map(Number);
-        d[1] -= 1;
-        d[2] -= 1;
-        d[2] += plus;
-        d[1] += Math.floor(d[2] / 28);
-        d[2] = d[2] % 28;
-        d[1] >= 12 ? (
-            d[0] += Math.floor(d[1] / 12),
-            d[1] = d[1] % 12
-        ) : -1;
-        d[1] += 1;
-        d[2] += 1;
-        
-        return d;
-    }
 }
