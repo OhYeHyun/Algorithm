3be8cf2e5b153e430485131cedc300b36591e3b0
OhYeHyun <107532928+OhYeHyun@users.noreply.github.com>
2023-08-18 18:49:34 +0900

    [unrated] Title: 문자열 나누기, Time: 1.30 ms, Memory: 36.8 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/140108. 문자열 나누기/문자열 나누기.js
@@ -0,0 +1,20 @@
+function solution(s) {
+    s = s.split("");
+    let arrS = [];
+    let cor = 0;
+    let wrong = 0;
+    let cnt = 0;
+    let start = s[0];
+    
+    s.forEach((e, idx) => {
+        e !== start ? wrong++ : cor++
+        
+        wrong == cor ? (
+            start = s[idx + 1],
+            cor = 0,
+            wrong = 0,
+            cnt++
+        ) : (!s[idx + 1] ? cnt++ : -1);
+    })
+    return cnt;
+}
