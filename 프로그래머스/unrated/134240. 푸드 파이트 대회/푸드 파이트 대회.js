01d2110a106305e4f7967aaf2e055a51d02352ab
OhYeHyun <107532928+OhYeHyun@users.noreply.github.com>
2023-08-22 19:42:10 +0900

    [unrated] Title: 푸드 파이트 대회, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/134240. 푸드 파이트 대회/푸드 파이트 대회.js
+++ 프로그래머스/unrated/134240. 푸드 파이트 대회/푸드 파이트 대회.js
@@ -4,7 +4,7 @@ function solution(food) {
     food.shift();
     
     food.forEach((e, idx) => {
-        Math.floor(e / 2) == 0 ? -1 : answer += String(idx + 1).repeat(Math.floor(e / 2));
+        answer += String(idx + 1).repeat(Math.floor(e / 2));
     })
     answerR = [...answer].reverse().join('');
     answer = answer.concat(0, answerR)
