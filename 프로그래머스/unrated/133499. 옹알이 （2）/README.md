976aa3a2b30b89d323535ce92120c9345274ea36
OhYeHyun <107532928+OhYeHyun@users.noreply.github.com>
2023-08-24 19:28:36 +0900

    [unrated] Title: 옹알이 (2), Time: 0.24 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/unrated/133499. 옹알이 （2）/README.md
+++ 프로그래머스/unrated/133499. 옹알이 （2）/README.md
@@ -1,10 +1,10 @@
 # [unrated] 옹알이 (2) - 133499 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/133499#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/133499) 
 
 ### 성능 요약
 
-메모리: 33.9 MB, 시간: 7.25 ms
+메모리: 33.6 MB, 시간: 0.24 ms
 
 ### 구분
 
--- 프로그래머스/unrated/133499. 옹알이 （2）/옹알이 （2）.js
+++ 프로그래머스/unrated/133499. 옹알이 （2）/옹알이 （2）.js
@@ -1,38 +1,22 @@
 function solution(babbling) {
     let answer = 0;
     let arr = ["aya", "ye", "woo", "ma"];
-    let last = '';
-    let boolean = true;
-    let i = 0;
     
     babbling.forEach((e) => {
-        console.log("-*-", e)
-        boolean = true;
-        i = 0;
+        let boolean = true;
+        let i = 0;
+        let last = '';
         while (boolean) {
-            if (e.includes(arr[i]) && last !== arr[i] && arr[i][0] == e[0]) {
-                console.log("case1")
-                console.log(arr[i])
-                console.log("자르기 전 ", e);
-                e = e.replace(arr[i], "");
-                last = arr[i];
-                console.log("자른 후 ", e, "last :", last);
-                i = 0;
-                if (e == "") {
-                    console.log("case2")
-                    last = '';
-                    boolean = false;
-                    answer++;
-                    console.log("answer++ ", answer)
-                }
-            } else if (i >= 4) {
-                console.log("case3")
-                last = '';
-                boolean = false;
-            } else {
-                console.log("case4")
-                i++;
-            }
+            e.includes(arr[i]) && last !== arr[i] && arr[i][0] == e[0] ? (
+                e = e.replace(arr[i], ""),
+                last = arr[i],
+                i = 0,
+                e == "" ? (
+                    boolean = false,
+                    answer++ ) : -1
+            )   
+            : i >= 4 ? boolean = false 
+            : i++;
         }
     });
     return answer;
