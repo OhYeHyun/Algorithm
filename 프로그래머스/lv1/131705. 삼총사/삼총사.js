39bdf03fb7baf359167ec6a4dd3224a32488464e
OhYeHyun <107532928+OhYeHyun@users.noreply.github.com>
2023-08-28 19:16:24 +0900

    [level 1] Title: 삼총사, Time: 3.36 ms, Memory: 33.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/131705. 삼총사/삼총사.js
@@ -0,0 +1,13 @@
+function solution(number) {
+    let answer = 0;
+    
+    number.forEach((e, idx) => {
+        for (let i = idx + 1; i < number.length; i++ ) {
+            let key = (e + number[i]) * -1;
+            for (let j = i + 1; j < number.length; j++) {
+                number[j] == key ? (answer++, console.log(idx, i, j, "key : ", key)) : -1; 
+            }
+        }
+    })
+    return answer;
+}
