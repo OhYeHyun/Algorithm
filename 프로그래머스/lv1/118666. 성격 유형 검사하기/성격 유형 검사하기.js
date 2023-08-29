f5d2b2183beef09365e85e456ac58101afddfd89
OhYeHyun <107532928+OhYeHyun@users.noreply.github.com>
2023-08-29 19:51:49 +0900

    [level 1] Title: 성격 유형 검사하기, Time: 0.59 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv1/118666. 성격 유형 검사하기/성격 유형 검사하기.js
+++ 프로그래머스/lv1/118666. 성격 유형 검사하기/성격 유형 검사하기.js
@@ -4,14 +4,12 @@ function solution(survey, choices) {
     let arr = new Array(8).fill(0);
     
     survey.forEach((e, idx) => {
-        choices[idx] == 4 ? -1 : (
-            choices[idx] > 4 ?
-            arr[kind.indexOf(e[1])] += choices[idx] - 4 : arr[kind.indexOf(e[0])] += 4 - choices[idx]
-        );
+        let [a, b] = Array.from(e);
+        arr[kind.indexOf(choices[idx] > 4 ? b : a)] += Math.abs(choices[idx] - 4)
     })
     
     for (let i = 0; i < 8; i += 2) {
-        arr[i] >= arr[i + 1] ? answer += kind[i] : answer += kind[i + 1];
+         answer += kind[(arr[i] >= arr[i + 1] ? i : i + 1)];
     }
     
     return answer;
