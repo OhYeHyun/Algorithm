19d3d635cf7c8665ea8562b6fd32b4c8d8a78d8f
OhYeHyun <107532928+OhYeHyun@users.noreply.github.com>
2023-09-04 18:12:24 +0900

    [level 1] Title: 음양 더하기, Time: 0.10 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/76501. 음양 더하기/음양 더하기.js
+++ 프로그래머스/lv1/76501. 음양 더하기/음양 더하기.js
@@ -2,7 +2,7 @@ function solution(absolutes, signs) {
     let answer = 0;
     
     absolutes.forEach((e, idx) => {
-        signs[idx] == false ? answer -= e : answer += e;
+        signs[idx] ? answer += e : answer -= e;
     })
     
     return answer;
