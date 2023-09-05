67155f12007acf12b30605f7b5d767ee100a324c
OhYeHyun <107532928+OhYeHyun@users.noreply.github.com>
2023-09-05 20:00:15 +0900

    [level 1] Title: 신규 아이디 추천, Time: 0.18 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/72410. 신규 아이디 추천/신규 아이디 추천.js
+++ 프로그래머스/lv1/72410. 신규 아이디 추천/신규 아이디 추천.js
@@ -1,20 +1,16 @@
 function solution(new_id) {
     let answer = '';
     
-    new_id = new_id.toLowerCase();    
+    answer = new_id
+        .toLowerCase()
+        .replace(/[^-_.a-z0-9]/g, "")
+        .replace(/\.+/g, ".")
+        .replace(/^\./g, "")
+        .replace(/^$/g, "a")
+        .substring(0, 15)
+        .replace(/\.$/g, "");
 
-    new_id = new_id.replace(/[^-_.a-z0-9]/g, "");
-    new_id = new_id.replace(/\.+/g, ".");
-    new_id = new_id.replace(/^\./g, "");
-    
-    if (!new_id) new_id = "a";
-    
-    new_id = new_id.substring(0, 15);
-    new_id = new_id.replace(/\.$/g, "");
-    
-    while (new_id.length <= 2) new_id.length > 3 ? -1 : new_id += new_id.at(-1);
-    
-    answer = new_id;
+    answer.length < 3 ? answer += answer.at(-1).repeat(3 - answer.length) : -1;
     
     return answer;
 }
