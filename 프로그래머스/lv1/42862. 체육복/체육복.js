82d73aadd1d32085a0261c0efb86fd7497e95b5b
OhYeHyun <107532928+OhYeHyun@users.noreply.github.com>
2023-09-14 19:10:38 +0900

    [level 1] Title: 체육복, Time: 0.10 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/42862. 체육복/체육복.js
+++ 프로그래머스/lv1/42862. 체육복/체육복.js
@@ -1,16 +1,15 @@
 function solution(n, lost, reserve) {
     let answer = n - lost.length;
     lost.sort((a, b) => a - b);
+    reserve.sort((a, b) => a - b);
 
     lost = lost.filter((e) => {
         return reserve.includes(e) ? (reserve.splice(reserve.indexOf(e), 1), answer++, false) : true;
     })
 
     lost.forEach((e) => {
-        reserve.includes(e - 1) ? 
-            (reserve.splice(reserve.indexOf(e - 1), 1), answer++) : 
-            (reserve.includes(e + 1) ? 
-                (reserve.splice(reserve.indexOf(e + 1), 1), answer++) : -1);
+        let eNum = reserve.find((a) => Math.abs(e - a) == 1);
+        !!eNum ? (reserve.splice(reserve.indexOf(eNum), 1), answer++) : -1;
     });
     
     return answer;
