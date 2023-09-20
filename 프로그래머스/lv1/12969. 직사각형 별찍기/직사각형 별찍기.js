70b2f504240c04e3b52d84be2e596cbf7a0ccd4e
OhYeHyun <107532928+OhYeHyun@users.noreply.github.com>
2023-09-20 18:16:00 +0900

    [level 1] Title: 직사각형 별찍기, Time: 50.06 ms, Memory: 32.1 MB -BaekjoonHub

--- 프로그래머스/lv1/12969. 직사각형 별찍기/직사각형 별찍기.js
+++ 프로그래머스/lv1/12969. 직사각형 별찍기/직사각형 별찍기.js
@@ -3,8 +3,6 @@ process.stdin.on('data', data => {
     const n = data.split(" ");
     const a = +n[0], b = +n[1];
     
-    for (let i = 1; i <= b; i++) {
-        console.log("*".repeat(a))
-    }
-    
+    console.log(("*".repeat(a) + '\n').repeat(b));
+
 });
