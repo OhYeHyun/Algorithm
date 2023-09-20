e4c761aa36411799e03d91befb1211901b699b8c
OhYeHyun <107532928+OhYeHyun@users.noreply.github.com>
2023-09-20 17:34:55 +0900

    [level 1] Title: 예산, Time: 0.12 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12982. 예산/예산.js
+++ 프로그래머스/lv1/12982. 예산/예산.js
@@ -1,11 +1,5 @@
 function solution(d, budget) {
-    let answer = 0, sum = 0;
-    d.sort((a, b) => a - b);
-    
-    d.forEach((e, idx) => {
-        sum += e;
-        sum <= budget ? answer = idx + 1 : -1;
-    })
-    
-    return answer;
+    return d.sort((a, b) => a - b).reduce((count, price) => {
+        return count + ((budget -= price) >= 0);
+    }, 0);
 }
