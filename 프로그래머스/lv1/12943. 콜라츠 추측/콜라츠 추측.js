7ac0a8978d6d52a6e77e8a2029a2c210870648ee
OhYeHyun <107532928+OhYeHyun@users.noreply.github.com>
2023-09-21 19:40:18 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.12 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12943. 콜라츠 추측/콜라츠 추측.js
@@ -0,0 +1,9 @@
+function solution(num) {
+    let answer = 0;
+    
+    while (num !== 1 && answer <= 500) {
+        num % 2 == 0 ? (num = num / 2, answer++) : (num = (num * 3) + 1, answer++);
+    }
+    
+    return answer > 500 ? -1 : answer;
+}
