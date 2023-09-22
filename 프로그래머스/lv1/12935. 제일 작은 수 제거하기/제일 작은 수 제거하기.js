3145d232e2168b11c49ea894afcba8175fa41f13
OhYeHyun <107532928+OhYeHyun@users.noreply.github.com>
2023-09-22 18:26:31 +0900

    [level 1] Title: 제일 작은 수 제거하기, Time: 0.04 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.js
+++ 프로그래머스/lv1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.js
@@ -1,6 +1,5 @@
 function solution(arr) {
-    let arrCopy = [...arr];
-    arr.splice(arr.indexOf(arrCopy.sort((a, b) => b - a).at(-1)), 1);
+    arr.splice(arr.indexOf(Math.min(...arr)), 1);
     
     return arr.length == 0 ? [-1] : arr;
 }
