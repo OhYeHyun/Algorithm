e48f41b21a1fe53f8a711f1e8985c02ff64f792c
OhYeHyun <107532928+OhYeHyun@users.noreply.github.com>
2023-09-22 18:04:47 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 0.19 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.js
+++ 프로그래머스/lv1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.js
@@ -5,10 +5,9 @@ function solution(n, m) {
     for (let i = 1; i <= Math.sqrt(max); i++) {
         max % i == 0 ? (i == Math.sqrt(max) ? arr.push(i) : (arr.push(i), arr.push(max / i))) : -1;
     }
-    
     arr = arr.sort((a, b) => b - a).filter((e) => min % e == 0);
     answer.push(arr[0]);
-    answer.push(arr[0] * ((max / arr[0]) * (min / arr[0])));
+    answer.push((max * min / arr[0]));
     
     return answer;
 }
