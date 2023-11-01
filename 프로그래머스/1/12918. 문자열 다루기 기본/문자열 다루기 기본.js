fef7849f41479eef262620f0bdcb492aaea7cd66
OhYeHyun <107532928+OhYeHyun@users.noreply.github.com>
2023-11-01 16:49:49 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.js
+++ 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.js
@@ -3,6 +3,6 @@ function solution(s) {
     
     s.length == 4 || s.length == 6 
         ? (s.split("").filter((e) => !isNaN(e)).join("") == s ? -1 : answer = false) : answer = false;
-    
+
     return answer;
 }
