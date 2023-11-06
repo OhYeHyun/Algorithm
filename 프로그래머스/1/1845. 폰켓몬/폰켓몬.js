a6b5a50cd65a4cbbff2d144156f48fa07ecbe808
OhYeHyun <107532928+OhYeHyun@users.noreply.github.com>
2023-11-06 16:17:08 +0900

    [level 1] Title: 폰켓몬, Time: 0.33 ms, Memory: 33.8 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.js
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.js
@@ -1,10 +1,6 @@
 function solution(nums) {
     let answer = 0;
-    let set = new Set();
-    
-    for (let i = 0; i < nums.length; i++) {
-        set.add(nums[i]);
-    }
+    let set = new Set([...nums]);
     
     answer = set.size > nums.length / 2 ? nums.length / 2 : set.size;
     
