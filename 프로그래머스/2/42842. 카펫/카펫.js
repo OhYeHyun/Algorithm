bb9f0a3a2da0f0540fce0ad4564662e6e54eddb5
OhYeHyun <107532928+OhYeHyun@users.noreply.github.com>
2023-11-10 18:30:15 +0900

    [level 2] Title: 카펫, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.js
+++ 프로그래머스/2/42842. 카펫/카펫.js
@@ -2,7 +2,6 @@ function solution(brown, yellow) {
     let answer = [];
     let AplusB = (brown + 4) / 2;
     let AtimesB = brown + yellow;
-    const num = Math.floor(Math.sqrt(AtimesB));
     
     for (let a = 3; a <= 5000; a++) {
         if (a * (AplusB - a) == AtimesB) {
