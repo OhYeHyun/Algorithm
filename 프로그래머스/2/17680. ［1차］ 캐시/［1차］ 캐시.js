ad63eca5167a1c08fe7b52f6b5d4b9681df7811e
OhYeHyun <107532928+OhYeHyun@users.noreply.github.com>
2023-12-07 19:11:53 +0900

    [level 2] Title: [1차] 캐시, Time: 0.74 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.js
+++ 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.js
@@ -1,11 +1,17 @@
 function solution(cacheSize, cities) {
-    let answer = 0;
-    let arr = [];
+    let answer = 0, arr = [];
     
     cities.forEach((e) => {
         e = e.toLowerCase()
-        arr.includes(e) ? (arr.splice(arr.indexOf(e), 1), arr.push(e), answer += 1) 
-        : (arr.push(e), answer += 5)
+        
+        if (arr.includes(e)) {
+            arr.splice(arr.indexOf(e), 1);
+            answer += 1;
+        } else {
+            answer += 5
+        }
+        arr.push(e);
+        
         arr.length > cacheSize ? arr.shift() : -1;
     })
     
