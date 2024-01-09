ac06931120cbb05604dc9caba42999c132c17faa
OhYeHyun <107532928+OhYeHyun@users.noreply.github.com>
2024-01-09 18:31:53 +0900

    [level unrated] Title: [PCCP 기출문제] 1번 / 붕대 감기, Time: 0.20 ms, Memory: 33.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/250137. ［PCCP 기출문제］ 1번 ／ 붕대 감기/［PCCP 기출문제］ 1번 ／ 붕대 감기.js
@@ -0,0 +1,34 @@
+function solution(bandage, health, attacks) {
+    let answer = 0;
+    let last = attacks.at(-1)[0];
+    let hp = health;
+    let success = 0;
+    
+    for (let i = 1; i <= last; i++) {
+        let attack = attacks[0];
+              
+        if (i == attack[0]) {
+            hp -= attack[1];
+            success = 0;
+            attacks.shift();
+        } else {
+            success += 1;
+            hp += bandage[1];
+            if (bandage[0] == success) {
+                hp += bandage[2];
+                success = 0;
+            }
+        }
+        
+        if (hp > health) hp = health;
+        
+        if (hp <= 0) {
+            hp = -1;
+            break;
+        }
+    }
+    
+    answer = hp;
+    
+    return answer;
+}
