dc15206b462b8263fb8a78afe264f2b94143488d
OhYeHyun <107532928+OhYeHyun@users.noreply.github.com>
2024-01-10 19:19:22 +0900

    [level 2] Title: 튜플, Time: 0.15 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/64065. 튜플/튜플.js
+++ 프로그래머스/2/64065. 튜플/튜플.js
@@ -1,12 +1,8 @@
 function solution(s) {
-    let ss = s.split(/{|}/).filter((item) => item !== "," && item !== "").sort((a, b) => a.length - b.length);
-    let answer = [];
+    let ss = s.split(/{|}/).filter((item) => item !== "," && item !== "").sort((a, b) => a.length - b.length)
+    let answer = new Set();
     
-    for (let i = 0; i < ss.length; i++) {
-        let arr = ss[i].split(",").map((item) => + item)
-        answer.push(...arr)
-    }
-    answer = [...new Set([...answer])];
+    ss.forEach((e) => e.split(",").forEach((ee) => answer.add(+ee)))
     
-    return answer;
+    return [...answer];
 }
