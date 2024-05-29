ee3017b61a5016e290bcabe98cc4ab928214e7c2
OhYeHyun <107532928+OhYeHyun@users.noreply.github.com>
2024-05-29 18:29:25 +0900

    [level 2] Title: [3차] n진수 게임, Time: 1.67 ms, Memory: 33.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.js
@@ -0,0 +1,13 @@
+function solution(n, t, m, p) {
+    let answer = '';
+    let strNum = '';
+    let num = 0;
+    
+    while (strNum.length <= t * m) {
+        strNum += (num++).toString(n).toUpperCase();
+    }
+    
+    answer = strNum.split('').filter((_, i) => i % m == (p - 1)).slice(0, t).join('');
+    
+    return answer;
+}
