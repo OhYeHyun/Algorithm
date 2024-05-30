b45f88c45465c48fe2a662c885b4c170088006bf
OhYeHyun <107532928+OhYeHyun@users.noreply.github.com>
2024-05-30 21:20:49 +0900

    [level 2] Title: 모음 사전, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.js
@@ -0,0 +1,22 @@
+function solution(word) {
+    let answer = 0;
+    const dir = new Map([['A', 1], ['E', 2], ['I', 3], ['O', 4], ['U', 5]]);
+        
+    for (let i = 0; i < word.length; i++) {
+        let curW = word[i];
+        let curN = dir.get(curW);
+        
+        let n = 5 - i;
+        for (let j = 0; j < n; j++) {
+            answer += (curN - 1) * 5 ** j;
+        }
+        
+        answer++;
+        
+    }
+    
+    return answer;
+
+}
+
+
