6d7cb3751e39b32abfd6fcdff3abf0395a4bee82
Oh Yehyun <107532928+OhYeHyun@users.noreply.github.com>
2024-06-11 17:28:00 +0900

    [goormlevel] Title: 장마, Time: 0.47 ms, Memory: 42.08 MB, Difficulty: 2 -BaekjoonHub

---
+++ goormlevel/194982/1. 장마/장마.js
@@ -0,0 +1,31 @@
+const readline = require('readline');
+let rl = readline.createInterface({
+	input: process.stdin,
+	output: process.stdout,
+});
+let input = [];
+rl.on('line', (line) => {
+  input.push(line.trim().split(" ").map((e) => +e));
+  if (input.length === Number(input[0][1]) + 2) {
+    rl.close();
+  }
+});
+rl.on('close', () => {
+	const [houseN, rainM] = input.shift();
+	let answer = input.shift();
+	let rain = new Array(houseN).fill(0);
+	
+	for (let i = 0; i < input.length; i++) {
+		let [first, last] = input[i];
+		for (let j = first - 1; j <= last - 1; j++) {
+			rain[j]++;
+		}
+		if ((i + 1) % 3 === 0) {
+			rain = rain.map((e) => e && e - 1);
+			answer = answer.map((e, idx) => e + rain[idx]);
+			rain.fill(0);
+		}
+	}
+	answer = answer.map((e, idx) => e + rain[idx]);
+	console.log(answer.join(" "));
+});
