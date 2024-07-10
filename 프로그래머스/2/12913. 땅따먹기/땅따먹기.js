34f5e0a54efbc8920602724a450ca28446dec860
Oh Yehyun <107532928+OhYeHyun@users.noreply.github.com>
2024-07-10 23:41:46 +0900

    [level 2] Title: 땅따먹기, Time: 39.02 ms, Memory: 72.5 MB -BaekjoonHub

--- 프로그래머스/2/12913. 땅따먹기/땅따먹기.js
+++ 프로그래머스/2/12913. 땅따먹기/땅따먹기.js
@@ -1,14 +1,18 @@
 function solution(land) {
-    let answer = [...land[0]]
+    let answer = [...land[0]];
 
     for (let i = 1; i < land.length; i++) {
-        let temp = new Array(4).fill(0);
-        
+        let temp = new Array(4);
+
         for (let j = 0; j < 4; j++) {
-            temp[j] += Math.max(...answer.filter((_, idx) => idx !== j)) + land[i][j]
+            temp[j] = land[i][j] + Math.max(
+                answer[(j + 1) % 4],
+                answer[(j + 2) % 4],
+                answer[(j + 3) % 4]
+            );
         }
-        answer = temp
+        answer = temp;
     }
-        
+
     return Math.max(...answer);
 }
