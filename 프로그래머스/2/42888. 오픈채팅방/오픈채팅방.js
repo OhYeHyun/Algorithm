6f7712c68d4888605eab0f7f969dc819b44087e3
Oh Yehyun <107532928+OhYeHyun@users.noreply.github.com>
2024-09-04 17:13:31 +0900

    [level 2] Title: 오픈채팅방, Time: 200.93 ms, Memory: 113 MB -BaekjoonHub

---
+++ 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.js
@@ -0,0 +1,28 @@
+function solution(record) {
+    const answer = [];
+    const userMap = new Map();
+    const commandMap = {"Enter" : "들어왔습니다.", "Leave" : "나갔습니다."};
+    
+    record.forEach((message) => {
+        const [command, id, name] = message.split(" ");
+        
+        if (command === 'Change') {
+            userMap.set(id, name);
+        }
+        
+        if (commandMap[command]) {
+            if (command === 'Enter') {
+                userMap.set(id, name);
+            }
+            answer.push([id, `님이 ${commandMap[command]}`])
+        }
+    })
+    
+    return answer.map((user) => {
+        const id = user[0]
+        const name = userMap.get(id);
+        user[0] = name;
+    
+        return user.join('');
+    })
+}
