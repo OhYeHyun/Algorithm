a64c7e4d10a6a9eb1291bf59b9205ae32f6457cf
Oh Yehyun <107532928+OhYeHyun@users.noreply.github.com>
2024-09-09 23:16:57 +0900

    [level 2] Title: 숫자 변환하기, Time: 401.86 ms, Memory: 122 MB -BaekjoonHub

---
+++ 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.js
@@ -0,0 +1,26 @@
+function solution(x, y, n) {
+    let answer = 0;
+    const expressions = [["+", n], ["*", 2], ["*", 3]];
+    const xSet = new Set();
+    
+    let queueIdx = 0;
+    const queue = [[x, answer]];
+    
+    while (queueIdx < queue.length) {
+        let [curX, curAnswer] = queue[queueIdx++];
+        if (curX === y) return curAnswer;
+        
+        for (const ex of expressions) {
+            const [operator, num] = ex;
+            let newX = curX;
+            if (operator === "+") newX += num;
+            if (operator === "*") newX *= num;
+
+            if (newX <= y && !xSet.has(newX)) {
+                xSet.add(newX);
+                queue.push([newX, curAnswer + 1]);
+            }
+        }
+    }
+    return -1;
+}
