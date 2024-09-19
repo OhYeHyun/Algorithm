3ab0c82b3c19c08a9c856b152a7b7cde3e325495
Oh Yehyun <107532928+OhYeHyun@users.noreply.github.com>
2024-09-19 20:26:48 +0900

    [level 2] Title: 가장 큰 수, Time: 225.04 ms, Memory: 44.9 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.js
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.js
@@ -2,14 +2,9 @@ function solution(numbers) {
     numbers.sort((a, b) => {
         const strA = a.toString();
         const strB = b.toString();
-        
-        const strAB = strA + strB;
-        const strBA = strB + strA;
 
-        return strBA - strAB;
+        return (strB + strA) - (strA + strB);
     });
-    
-    const sum = numbers.reduce((acc, cur) => acc + cur, 0)
-   
-    return sum === 0 ? "0" : numbers.join("");
+
+    return numbers[0] === 0 ? "0" : numbers.join("");
 }
