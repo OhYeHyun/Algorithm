853eb28cfee16f0afc1086999c3da458c494144a
Oh Yehyun <107532928+OhYeHyun@users.noreply.github.com>
2024-10-04 21:47:45 +0900

    [level 2] Title: 두 큐 합 같게 만들기, Time: 44.18 ms, Memory: 155 MB -BaekjoonHub

--- 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.java
+++ 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.java
@@ -1,45 +1,52 @@
 import java.util.*;
 
-public class Solution {
-    public static int solution(int[] queue1, int[] queue2) {
-        int score = 0;
-        int total = queue1.length * 3 - 1;
+class Solution {
+    static int func(Deque<Integer> q1, Deque<Integer> q2, long sum1, long sum2){
+        int cnt = 0;
+        long targ = (sum1 + sum2) / 2;
+        int len = q1.size() * 3;
+        int idx = 0;
+        
+        while (idx < len) {
+            idx++;
+            
+            if (sum1 > sum2) {
+                int val = q1.poll();
+                q2.add(val);
+                sum1 -= val;
+                sum2 += val;
+                cnt++;
+            } else if (sum1 < sum2){
+                int val = q2.poll();
+                q1.add(val);
+                sum1 += val;
+                sum2 -= val;
+                cnt++;
+            }
+            else return cnt;
+        }
+        return -1;
+    }
+    
+    public int solution(int[] queue1, int[] queue2) {
+        int answer = -2;
+
+        long sum1 = 0;
+        long sum2 = 0;
+        
+        Deque<Integer> q1 = new ArrayDeque<>();
+        Deque<Integer> q2 = new ArrayDeque<>();
 
         for (int i = 0; i < queue1.length; i++) {
-            score += (queue1[i] + queue2[i]);
+            sum1 += queue1[i];
+            sum2 += queue2[i];
+            q1.add(queue1[i]);
+            q2.add(queue2[i]);
         }
         
-        if (score % 2 == 1) return -1;
-        score /= 2;
+        if (sum1 == sum2) return 0;
+        answer = func(q1,q2,sum1,sum2);
 
-        int enCount = 0;
-        int deCount = 0;
-        int sumQ1 = Arrays.stream(queue1).sum();
-
-        Queue<Integer> q1 = new LinkedList<>();
-        Queue<Integer> q2 = new LinkedList<>();
-
-        for (int i : queue1) q1.add(i);
-        for (int i : queue2) q2.add(i);
-
-        while (enCount + deCount < total) {
-            if (sumQ1 > score) {
-                int value = q1.poll();
-                q2.add(value);
-                deCount++;
-                sumQ1 -= value;
-            } else if (sumQ1 < score) {
-                int value = q2.poll();
-                q1.add(value);
-                enCount++;
-                sumQ1 += value;
-            } else {
-                // sumQ1 == score
-                int sumQ2 = q2.stream().mapToInt(Integer::intValue).sum(); 
-                return sumQ2 == score ? enCount + deCount : -1;
-            }
-        }
-
-        return -1;
+        return answer;
     }
 }
