c428e1b3fc2d192ac9b3d5feb1a32cd80b74f17c
Oh Yehyun <107532928+OhYeHyun@users.noreply.github.com>
2024-12-20 23:41:06 +0900

    [level 2] Title: 연속된 부분 수열의 합, Time: 10.92 ms, Memory: 137 MB -BaekjoonHub

--- 프로그래머스/2/178870. 연속된 부분 수열의 합/연속된 부분 수열의 합.java
+++ 프로그래머스/2/178870. 연속된 부분 수열의 합/연속된 부분 수열의 합.java
@@ -1,37 +1,26 @@
-import java.util.*;
-
 class Solution {
     public int[] solution(int[] sequence, int k) {
-        int[] answer = new int[2];
-        int[] prefix = new int[sequence.length + 1];
-        int maxLength = Integer.MAX_VALUE;
+        int[] result = new int[2];
+        int left = 0;
+        int right = 0;
+        int sum = 0;
+        int minLength = Integer.MAX_VALUE;
 
-        for (int i = 0; i < sequence.length; i++) {
-            prefix[i + 1] = prefix[i] + sequence[i];
-        }
-        // sequence = [1, 2, 3, 4, 5] → prefix = [0, 1, 3, 6, 10, 15]
+        while (right < sequence.length) {
+            sum += sequence[right];
 
-        int left = 0, right = 0;
-
-        while (left <= right && right <= sequence.length) {
-            int sum = prefix[right] - prefix[left];
-            if (sum == k) {
-
-                int curLength = right - left - 1;
-                if (maxLength > curLength) {
-                    answer[0] = left;
-                    answer[1] = right - 1;
-                    maxLength = curLength;
+            while (sum >= k) {
+                if (sum == k && (right - left) < minLength) {
+                    minLength = right - left;
+                    result[0] = left;
+                    result[1] = right;
                 }
-            }
-            
-            if (sum < k) {
-                right++;
-            } else {
+                sum -= sequence[left];
                 left++;
             }
+            right++;
         }
 
-        return answer;
+        return result;
     }
 }
