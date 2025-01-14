5079c048e1d41873fd7b2b01027f60fe37fb2f93
Oh Yehyun <107532928+OhYeHyun@users.noreply.github.com>
2025-01-14 21:00:37 +0900

    [level 2] Title: 배달, Time: 7.09 ms, Memory: 87.8 MB -BaekjoonHub

--- 프로그래머스/2/12978. 배달/README.md
+++ 프로그래머스/2/12978. 배달/README.md
@@ -1,10 +1,10 @@
 # [level 2] 배달 - 12978 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12978#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12978) 
 
 ### 성능 요약
 
-메모리: 73 MB, 시간: 16.29 ms
+메모리: 87.8 MB, 시간: 7.09 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 01월 13일 17:30:53
+2025년 01월 14일 21:00:34
 
 ### 문제 설명
 
--- 프로그래머스/2/12978. 배달/배달.java
+++ 프로그래머스/2/12978. 배달/배달.java
@@ -5,40 +5,43 @@ class Solution {
         int[] distance = new int[N + 1];
         Arrays.fill(distance, Integer.MAX_VALUE);
         distance[1] = 0;
-
+        
         List<int[]>[] graph = new ArrayList[N + 1];
+        
         for (int i = 1; i <= N; i++) {
             graph[i] = new ArrayList<>();
         }
+        
         for (int[] r : road) {
             graph[r[0]].add(new int[]{r[1], r[2]});
             graph[r[1]].add(new int[]{r[0], r[2]});
         }
-
-        PriorityQueue<int[]> pq = new PriorityQueue<>(Comparator.comparingInt(a -> a[1]));
-        pq.add(new int[]{1, 0});
-
-        while (!pq.isEmpty()) {
-            int[] current = pq.poll();
-            int node = current[0];
-            int dist = current[1];
-
-            if (dist > distance[node]) continue;
-
-            for (int[] neighbor : graph[node]) {
-                int nextNode = neighbor[0];
-                int cost = neighbor[1];
-                if (distance[nextNode] > distance[node] + cost) {
-                    distance[nextNode] = distance[node] + cost;
-                    pq.add(new int[]{nextNode, distance[nextNode]});
+        
+        PriorityQueue<int[]> queue = new PriorityQueue<>(Comparator.comparingInt(a -> a[1]));
+        queue.add(new int[]{1, 0});
+        
+        while (!queue.isEmpty()) {
+            int[] cur = queue.poll();
+            int curNode = cur[0];
+            int curCost = cur[1];
+            
+            if (curCost > distance[curNode]) continue;
+            
+            for (int[] next : graph[curNode]) {
+                int nextNode = next[0];
+                int nextCost = next[1];
+                
+                if (distance[nextNode] > curCost + nextCost) {
+                    distance[nextNode] = curCost + nextCost;
+                    queue.add(new int[]{nextNode, distance[nextNode]});
                 }
             }
         }
-
-        int answer = 0;
-        for (int d : distance) {
-            if (d <= K) answer++;
-        }
+        
+        int answer = (int) Arrays.stream(distance)
+            .filter(d -> d <= K)
+            .count();
+        
         return answer;
     }
 }
