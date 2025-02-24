af4bfb3023554b0925fc1f9e58fdd339bca0779e
Oh Yehyun <107532928+OhYeHyun@users.noreply.github.com>
2025-02-24 16:58:05 +0900

    [level 2] Title: 서버 증설 횟수, Time: 0.08 ms, Memory: 73.9 MB -BaekjoonHub

---
+++ Java/프로그래머스/2/389479. 서버 증설 횟수/서버 증설 횟수.java
@@ -0,0 +1,34 @@
+import java.util.*;
+import java.util.stream.Collectors;
+
+class Solution {
+    public int solution(int[] players, int m, int k) {
+        int answer = 0;
+        List<int[]> servers = new ArrayList<>();
+        int curServer = 0;
+        
+        for (int player : players) {
+            int targetServer = player / m;
+            if (targetServer > curServer) {
+                int newServers = targetServer - curServer;
+                servers.add(new int[]{newServers, k});
+                answer += newServers;
+                curServer += newServers;
+            }
+            
+            List<int[]> updatedServers = new ArrayList<>();
+            for (int[] server : servers) {
+                server[1]--;
+                if (server[1] > 0) {
+                    updatedServers.add(server);
+                } else {
+                    curServer -= server[0];
+                }
+            }
+            
+            servers = updatedServers;
+        }  
+        
+        return answer;
+    }
+}
