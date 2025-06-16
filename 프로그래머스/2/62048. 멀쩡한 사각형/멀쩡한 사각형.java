29e3875578f4652da2aac5a8b006e0f08617c10a
Oh Yehyun <107532928+OhYeHyun@users.noreply.github.com>
2025-06-16 20:22:13 +0900

    [level 2] Title: 멀쩡한 사각형, Time: 1.17 ms, Memory: 79.6 MB -BaekjoonHub

--- 프로그래머스/2/62048. 멀쩡한 사각형/멀쩡한 사각형.java
+++ 프로그래머스/2/62048. 멀쩡한 사각형/멀쩡한 사각형.java
@@ -1,21 +1,12 @@
-import java.util.*;
+import java.math.BigInteger;
 
 class Solution {
     public long solution(int w, int h) {
         long total = (long) w * h;
         
-        long gcd = getGCD(w, h);
+        long gcd = BigInteger.valueOf(w).gcd(BigInteger.valueOf(h)).longValue();
         long unusable = w + h - gcd;
         
         return total - unusable;
-    }
-
-    private long getGCD(int a, int b) {
-        while (b != 0) {
-            int tmp = a % b;
-            a = b;
-            b = tmp;
-        }
-        return a;
     }
 }
