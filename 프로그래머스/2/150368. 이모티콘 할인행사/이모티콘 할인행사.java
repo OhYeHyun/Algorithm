ae20e219661c217008a1f35c31d20b3dc9d1aa01
Oh Yehyun <107532928+OhYeHyun@users.noreply.github.com>
2025-08-15 16:08:57 +0900

    [level 2] Title: 이모티콘 할인행사, Time: 46.50 ms, Memory: 79.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/150368. 이모티콘 할인행사/이모티콘 할인행사.java
@@ -0,0 +1,60 @@
+import java.util.*;
+
+class Solution {
+    private int[] answer = new int[2];
+    private int[][] users;
+    private int[] emoticons;
+    
+    public int[] solution(int[][] users, int[] emoticons) {
+        this.users = users;
+        this.emoticons = emoticons;
+        
+        int[] rate = new int[emoticons.length];
+        dfs(0, rate);
+                
+        return answer;
+    }
+    
+    private void dfs(int idx, int[] rate) {
+        if (idx == rate.length) {
+            int[] result = cal(users, emoticons, rate);
+            if (answer[0] < result[0] || (answer[0] == result[0] && answer[1] < result[1])) {
+                answer = result;
+            }
+            return;
+        }
+        
+        for (int j = 10; j <= 40; j += 10) {
+            rate[idx] = j;
+            dfs(idx + 1, rate);
+        }
+    }
+    
+    private int[] cal(int[][] users, int[] emoticons, int[] rate) {
+        int[] answer = new int[2];
+        
+        for (int[] user : users) {
+            double buy = 0;
+            boolean plus = false;
+            
+            for (int i = 0; i < emoticons.length; i++) {
+                if (rate[i] >= user[0]) {
+                    buy += emoticons[i] * ((100 - rate[i]) * 0.01);
+                }
+                
+                if (buy >= user[1]) {
+                    plus = true;
+                    break;
+                }
+            }
+            
+            if (plus) {
+                answer[0] += 1;
+            } else {
+                answer[1] += (int) buy;
+            }
+        }
+        
+        return answer;
+    }
+}
