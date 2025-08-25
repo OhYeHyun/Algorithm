7f2aaf7d82c187d573a5d300c6d3ef02c109cd66
Oh Yehyun <107532928+OhYeHyun@users.noreply.github.com>
2025-08-25 15:28:00 +0900

    [level 3] Title: 정수 삼각형, Time: 9.94 ms, Memory: 62.6 MB -BaekjoonHub

---
+++ 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int solution(int[][] triangle) {
+        int answer = 0;
+        int y = triangle.length;
+        
+        int[][] dp = new int[y][y];
+        dp[0][0] = triangle[0][0];
+        
+        for (int i = 1; i < y; i++) {
+            for (int j = 0; j <= i; j++) {
+                if (j == 0) {
+                    dp[i][j] = dp[i - 1][j] + triangle[i][j];
+                } else if (j == i) {
+                    dp[i][j] = dp[i - 1][j - 1] + triangle[i][j];
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j - 1], dp[i - 1][j]) + triangle[i][j];
+                }
+            }
+        }
+        
+        for (int r : dp[y - 1]) {
+            answer = Math.max(answer, r);
+        }
+        
+        return answer;
+    }
+}
