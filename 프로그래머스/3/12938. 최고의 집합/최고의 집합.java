a00ed651c3634acf789fbe5090568fd085c57c9a
Oh Yehyun <107532928+OhYeHyun@users.noreply.github.com>
2025-10-09 17:13:27 +0900

    [level 3] Title: 최고의 집합, Time: 0.28 ms, Memory: 54.9 MB -BaekjoonHub

---
+++ 프로그래머스/3/12938. 최고의 집합/최고의 집합.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int n, int s) {
+        if (s < n) return new int[]{-1};
+        
+        int[] answer = new int[n];
+        int base = s / n;
+        int rest = s % n;
+
+        for (int i = 0; i < n; i++) {
+            if (i < n - rest) {
+                answer[i] = base;
+            } else {
+                answer[i] = base + 1;
+            }
+        }
+
+        return answer;
+    }
+}
