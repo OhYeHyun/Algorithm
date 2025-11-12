ad4a1daabc63ba59d70d6289b36c579041ab94d6
Oh Yehyun <107532928+OhYeHyun@users.noreply.github.com>
2025-11-12 22:14:33 +0900

    [level 3] Title: 스티커 모으기(2), Time: 6.73 ms, Memory: 58.2 MB -BaekjoonHub

--- 프로그래머스/3/12971. 스티커 모으기（2）/스티커 모으기（2）.java
+++ 프로그래머스/3/12971. 스티커 모으기（2）/스티커 모으기（2）.java
@@ -2,48 +2,30 @@ import java.util.*;
 
 class Solution {
     public int solution(int sticker[]) {
-        int answer = 0;
         int n = sticker.length;
+        
+        if (n == 1) {
+            return sticker[0];
+        }
+        if (n == 2) {
+            return Math.max(sticker[0], sticker[1]);
+        }
         
         int[] dp1 = new int[n];
         int[] dp2 = new int[n];
         
-        for (int i = 0; i < n; i++) {
-            if (i == 0) {
-                dp1[0] = sticker[0];
-                continue;
-            }
-            
-            if (i == 1) {
-                dp1[1] = dp1[0];
-                continue;
-            }
-            
-            if (i == n - 1) {
-                dp1[i] = dp1[i - 1];
-                continue;
-            }
-            
+        dp1[0] = sticker[0];
+        dp1[1] = Math.max(sticker[0], sticker[1]);
+        for (int i = 2; i < n - 1; i++) {
             dp1[i] = Math.max(dp1[i - 1], dp1[i - 2] + sticker[i]);
-        
         }
-        
-                
-        for (int i = 0; i < n; i++) {
-            if (i == 0) {
-                dp2[0] = 0;
-                continue;
-            }
-            
-            if (i == 1) {
-                dp2[1] = sticker[1];
-                continue;
-            }
 
+        dp2[0] = 0;
+        dp2[1] = sticker[1];
+        for (int i = 2; i < n; i++) {
             dp2[i] = Math.max(dp2[i - 1], dp2[i - 2] + sticker[i]);
         }
-        
 
-        return Math.max(dp1[n - 1], dp2[n - 1]);
+        return Math.max(dp1[n - 2], dp2[n - 1]);
     }
 }
