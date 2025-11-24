a22eb6f56e4a2269134ca7974d832893ff8e082d
Oh Yehyun <107532928+OhYeHyun@users.noreply.github.com>
2025-11-24 21:37:44 +0900

    [level 3] Title: 가장 먼 노드, Time: 79.98 ms, Memory: 115 MB -BaekjoonHub

--- 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
@@ -2,16 +2,16 @@ import java.util.*;
 
 class Solution {
     public int solution(int n, int[][] edge) {
-        Map<Integer, Set<Integer>> m = new HashMap<>();
+        Map<Integer, Set<Integer>> map = new HashMap<>();
         for (int[] e : edge) {
-            m.computeIfAbsent(e[0], k -> new HashSet<>()).add(e[1]);
-            m.computeIfAbsent(e[1], k -> new HashSet<>()).add(e[0]);
+            map.computeIfAbsent(e[0], k -> new HashSet<>()).add(e[1]);
+            map.computeIfAbsent(e[1], k -> new HashSet<>()).add(e[0]);
         }
         
-        int[] d = new int[m.size() + 1];
+        int[] distance = new int[n + 1];
         
-        boolean[] v = new boolean[m.size() + 1];
-        v[1] = true;
+        boolean[] visited = new boolean[n + 1];
+        visited[1] = true;
         
         Queue<Integer> q = new LinkedList<>();
         q.add(1);
@@ -19,19 +19,19 @@ class Solution {
         while (!q.isEmpty()) {
             Integer cur = q.poll();
             
-            for (Integer a : m.get(cur)) {
-                if (!v[a]) {
-                    v[a] = true;
-                    d[a] = d[cur] + 1;
+            for (Integer a : map.get(cur)) {
+                if (!visited[a]) {
+                    visited[a] = true;
+                    distance[a] = distance[cur] + 1;
                     q.add(a);
                 }
             }
         }
         
-        int max = Arrays.stream(d).max().getAsInt();
+        int max = Arrays.stream(distance).max().getAsInt();
         int count = 0;
-        for (int dd : d) {
-            if (dd == max) count++;
+        for (int d : distance) {
+            if (d == max) count++;
         }
 
         return count;
